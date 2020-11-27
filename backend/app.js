ba684af20efd5c14385a5f2726f8b5da8e8bac03
Alex Pavlov <halfer.orc@gmail.com>
2020-11-27 22:26:17 +0300

    fix(frnt, bck) api, логи

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
+app.options('*', cors());
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
